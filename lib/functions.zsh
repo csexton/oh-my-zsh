86c1140e06e125792754499a1a7aab0929334683
Christopher Sexton <csexton@gmail.com>
2010-06-19 12:27:39 -0400

    Removing tab function since it is not a plugin

--- lib/functions.zsh
+++ lib/functions.zsh
@@ -16,23 +16,6 @@ function upgrade_oh_my_zsh() {
   /bin/sh $ZSH/tools/upgrade.sh
 }
 
-function tab() {
-  if [[ $DESKTOP_SESSION == "gnome" ]]; then
-    # requires xdotool to be installed which is a simple: `sudo apt-get install xdotool`
-    nocorrect xdotool key ctrl+shift+t
-  else
-    osascript 2>/dev/null <<EOF
-      tell application "System Events"
-        tell process "Terminal" to keystroke "t" using command down
-      end
-      tell application "Terminal"
-        activate
-        do script with command "cd \"$PWD\"; clear; $*" in window 1
-      end tell
-EOF
-  fi
-}
-
 function take() {
   mkdir -p $1
   cd $1
